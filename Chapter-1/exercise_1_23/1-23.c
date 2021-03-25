aebcecc2a1ea8198e41eeac982729e971a038852
helious.wu <helious.wu@foxmail.com>
2021-03-25 21:51:24 +0800

    add solution to exercise 1-23

---
+++ Chapter-1/exercise_1_23/1-23.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int flag = 0; // default: uncomment
+
+int main(void)
+{
+    int eof, c, next, splash_flag, star_flag;
+    eof = splash_flag = star_flag = 0;
+    next = -2;
+    while ((eof == 0) && ((c = getchar()) != EOF))
+    {
+        if (!flag) // uncomment
+        {
+            if (c == '/')
+            {
+                if ((next = getchar()) == '/')
+                    splash_flag = 1;
+                else if (next == '*')
+                    star_flag = 1;
+                else if (next == EOF) // EOF
+                    eof = 1;
+            }
+            flag = splash_flag + star_flag;
+            if (flag == 0)
+            {
+                putchar(c);
+                if (next != -2)
+                    putchar(next);
+            }
+        }
+        else
+        {
+            if (c == '*')
+                if ((next = getchar()) == '/')
+                    star_flag = splash_flag = 0;
+                else if (next == EOF)
+                    eof = 1;
+            if (c == '\n' || next == '\n')
+                splash_flag = 0;
+            flag = star_flag + splash_flag;
+        }
+        // printf("\nflag status:\nstar_flag:%d splash_flag:%d\n", star_flag, splash_flag);
+        // printf("eof status:%d\n", eof);
+        next = -2;
+    }
+}
+
+/* 默认为未注释状态
+ * 情况处理：
+ *    * 未注释：读取一个字符，输出一个字符
+ *    *   注释：读取字符，不输出
+ * 
+ * 注释状态细分：
+ *   1. "//"：则从此刻开始，直至遇到'\n'结束注释
+ *   2. "/*": 从此刻开始，直至反向结束
+ * 
+ */
+
+// Specail Case:
+/*/dd
+/* /*/
